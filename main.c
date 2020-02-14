c8bf9d208ed69d68a38f02d1167e85fe48038bf9
Raffaele <raffyt@hotmail.it>
2020-02-14 11:15:18 +0100

    aggiunta casi nsm

--- main.c
+++ main.c
@@ -20,63 +20,100 @@ void print_icmp_packet(const u_char * , int );
 void PrintData (const u_char * , int);
 int elaborate_packet(const u_char * , int);
 
-FILE *logfile;
 struct sockaddr_in source,dest;
 int tcp=0,udp=0,icmp=0,others=0,igmp=0,total=0,i,j;
 
-int main()
+int main(int argc, char **argv)
 {
+
+    if (argc < 2 || (strcmp(argv[1], "nsm") != 0 || strcmp(argv[1], "namespaces") != 0)) {
+        printf("Usage: nsm or namespaces\n");
+        return 0;
+    }
     pcap_if_t *alldevsp , *device;
     pcap_t *handle; //Handle of the device that shall be sniffed
 
     char errbuf[100] , *devname , devs[100][100];
     int count = 1 , n;
 
-    //First get the list of available devices
-    printf("Finding available devices ... ");
-    if( pcap_findalldevs( &alldevsp , errbuf) )
-    {
-        printf("Error finding devices : %s" , errbuf);
-        exit(1);
-    }
-    printf("Done");
-
-    //Print the available devices
-    printf("\nAvailable Devices are :\n");
-    for(device = alldevsp ; device != NULL ; device = device->next)
-    {
-        printf("%d. %s - %s\n" , count , device->name , device->description);
-        if(device->name != NULL)
+    if (strcmp(argv[1], "nsm") == 0) {
+        //First get the list of available devices
+        printf("Finding available devices ... ");
+        if( pcap_findalldevs( &alldevsp , errbuf) )
         {
-            strcpy(devs[count] , device->name);
+            printf("Error finding devices : %s" , errbuf);
+            exit(1);
         }
-        count++;
+        printf("Done");
+
+        //Print the available devices
+        printf("\nAvailable Devices are :\n");
+        for(device = alldevsp ; device != NULL ; device = device->next)
+        {
+            printf("%d. %s - %s\n" , count , device->name , device->description);
+            if(device->name != NULL)
+            {
+                if (strstr(device->name, "nsm"))
+                    devname = device->name;
+                strcpy(devs[count] , device->name);
+            }
+            count++;
+        }
+
+        //Open the device for sniffing
+        printf("Opening NSM device with clinet %s for sniffing ... " , devname);
+        handle = pcap_open_live(devname , 65536 , 1 , 0 , errbuf);
+
+        if (handle == NULL)
+        {
+            fprintf(stderr, "Couldn't open device %s : %s\n" , devname , errbuf);
+            exit(1);
+        }
+        printf("Done\n");
+
+        //Put the device in sniff loop
+        pcap_loop(handle , -1 , process_packet , NULL);
+    } else {
+        //First get the list of available devices
+        printf("Finding available devices ... ");
+        if( pcap_findalldevs( &alldevsp , errbuf) )
+        {
+            printf("Error finding devices : %s" , errbuf);
+            exit(1);
+        }
+        printf("Done");
+
+        //Print the available devices
+        printf("\nAvailable Devices are :\n");
+        for(device = alldevsp ; device != NULL ; device = device->next)
+        {
+            printf("%d. %s - %s\n" , count , device->name , device->description);
+            if(device->name != NULL)
+            {
+                strcpy(devs[count] , device->name);
+            }
+            count++;
+        }
+
+        //Ask user which device to sniff
+        printf("Enter the number of the device you want to sniff : ");
+        scanf("%d" , &n);
+        devname = devs[n];
+
+        //Open the device for sniffing
+        printf("Opening device %s for sniffing ... " , devname);
+        handle = pcap_open_live(devname , 65536 , 1 , 0 , errbuf);
+
+        if (handle == NULL)
+        {
+            fprintf(stderr, "Couldn't open device %s : %s\n" , devname , errbuf);
+            exit(1);
+        }
+        printf("Done\n");
+
+        //Put the device in sniff loop
+        pcap_loop(handle , -1 , process_packet , NULL);
     }
-
-    //Ask user which device to sniff
-    printf("Enter the number of the device you want to sniff : ");
-    scanf("%d" , &n);
-    devname = devs[n];
-
-    //Open the device for sniffing
-    printf("Opening device %s for sniffing ... " , devname);
-    handle = pcap_open_live(devname , 65536 , 1 , 0 , errbuf);
-
-    if (handle == NULL)
-    {
-        fprintf(stderr, "Couldn't open device %s : %s\n" , devname , errbuf);
-        exit(1);
-    }
-    printf("Done\n");
-
-    logfile=fopen("log.txt","w");
-    if(logfile==NULL)
-    {
-        printf("Unable to create file.");
-    }
-
-    //Put the device in sniff loop
-    pcap_loop(handle , -1 , process_packet , NULL);
 
     return 0;
 }
@@ -92,7 +129,7 @@ void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char
     {
         case 1:  //ICMP Protocol
             ++icmp;
-            print_icmp_packet( buffer , size);
+            elaborate_packet( buffer , size);
             break;
 
         case 2:  //IGMP Protocol
@@ -101,7 +138,7 @@ void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char
 
         case 6:  //TCP Protocol
             ++tcp;
-            print_tcp_packet(buffer , size);
+            elaborate_packet(buffer, size);
             break;
 
 //        case 17: //UDP Protocol
@@ -116,16 +153,6 @@ void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char
     printf("TCP : %d   UDP : %d   ICMP : %d   IGMP : %d   Others : %d   Total : %d\r", tcp , udp , icmp , igmp , others , total);
 }
 
-void print_ethernet_header(const u_char *Buffer, int Size)
-{
-    struct ethhdr *eth = (struct ethhdr *)Buffer;
-
-    printf("\n");
-    printf("Ethernet Header\n");
-    printf("   |-Destination Address : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X \n", eth->h_dest[0] , eth->h_dest[1] , eth->h_dest[2] , eth->h_dest[3] , eth->h_dest[4] , eth->h_dest[5] );
-    printf("   |-Source Address      : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X \n", eth->h_source[0] , eth->h_source[1] , eth->h_source[2] , eth->h_source[3] , eth->h_source[4] , eth->h_source[5] );
-    printf("   |-Protocol            : %u \n",(unsigned short)eth->h_proto);
-}
 
 int elaborate_packet(const u_char * Buffer, int Size)
 {
@@ -146,226 +173,4 @@ int elaborate_packet(const u_char * Buffer, int Size)
         return 1;
     }
 
-}
-
-void elaborate_and_print_ip_header(const u_char * Buffer, int Size)
-{
-
-    if (elaborate_packet(Buffer, Size) == 1) {
-        return;
-    }
-
-//    print_ethernet_header(Buffer , Size);
-
-//    unsigned short iphdrlen;
-
-    struct iphdr *iph = (struct iphdr *)(Buffer  + sizeof(struct ethhdr) );
-//    iphdrlen =iph->ihl*4;
-//
-//    memset(&source, 0, sizeof(source));
-//    source.sin_addr.s_addr = iph->saddr;
-//
-//    memset(&dest, 0, sizeof(dest));
-//    dest.sin_addr.s_addr = iph->daddr;
-
-    /*
-    printf("\n");
-    printf("IP Header\n");
-    printf("   |-IP Version        : %d\n",(unsigned int)iph->version);
-    printf("   |-IP Header Length  : %d DWORDS or %d Bytes\n",(unsigned int)iph->ihl,((unsigned int)(iph->ihl))*4);
-    printf("   |-Type Of Service   : %d\n",(unsigned int)iph->tos);
-    printf("   |-IP Total Length   : %d  Bytes(Size of Packet)\n",ntohs(iph->tot_len));
-    printf("   |-Identification    : %d\n",ntohs(iph->id));
-    printf("   |-TTL      : %d\n",(unsigned int)iph->ttl);
-    printf("   |-Protocol : %d\n",(unsigned int)iph->protocol);
-    printf("   |-Checksum : %d\n",ntohs(iph->check));
-    printf("   |-Source IP        : %s\n" , inet_ntoa(source.sin_addr) );
-    printf("   |-Destination IP   : %s\n" , inet_ntoa(dest.sin_addr) );
-    */
-
-//    pcap_t *fp;
-//    char errbuf[PCAP_ERRBUF_SIZE];
-//
-//    /* Open the output device */
-//    fp = pcap_open_live("veth2" , Size , 1 , 0 , errbuf);
-//
-//    if (fp == NULL)
-//    {
-//        fprintf(stderr, "Couldn't open device veth2 : %s\n", errbuf);
-//        exit(1);
-//    }
-
-//    printf("Opening device veth2 to send packets ... ");
-
-    u_char *packet= (u_char *)Buffer;
-
-//    print_ethernet_header(packet, Size);
-
-//    iph = (struct iphdr *)(packet  + sizeof(struct ethhdr) );
-//
-//    if (pcap_sendpacket(fp, packet, Size) != 0)
-//    {
-//        fprintf(stderr,"\nError sending the packet: %s\n", pcap_geterr(fp));
-//        return;
-//    }
-
-//    iph = (struct iphdr *)(packet  + sizeof(struct ethhdr) );
-//
-//    memset(&source, 0, sizeof(source));
-//    source.sin_addr.s_addr = iph->saddr;
-//
-//    memset(&dest, 0, sizeof(dest));
-//    dest.sin_addr.s_addr = iph->daddr;
-//
-//    printf("   |-Source IP        : %s\n" , inet_ntoa(source.sin_addr) );
-//    printf("   |-Destination IP   : %s\n" , inet_ntoa(dest.sin_addr) );
-//
-//    printf("Packet sent to dest interface.\n");
-}
-
-void print_tcp_packet(const u_char * Buffer, int Size)
-{
-    unsigned short iphdrlen;
-
-    struct iphdr *iph = (struct iphdr *)( Buffer  + sizeof(struct ethhdr) );
-    iphdrlen = iph->ihl*4;
-
-    struct tcphdr *tcph=(struct tcphdr*)(Buffer + iphdrlen + sizeof(struct ethhdr));
-
-    int header_size =  sizeof(struct ethhdr) + iphdrlen + tcph->doff*4;
-
-    fprintf(logfile,"\n***********************TCP Packet*************************\n");
-
-    elaborate_and_print_ip_header(Buffer,Size);
-
-    fprintf(logfile , "\n");
-    fprintf(logfile , "TCP Header\n");
-    fprintf(logfile , "   |-Source Port      : %u\n",ntohs(tcph->source));
-    fprintf(logfile , "   |-Destination Port : %u\n",ntohs(tcph->dest));
-    fprintf(logfile , "   |-Sequence Number    : %u\n",ntohl(tcph->seq));
-    fprintf(logfile , "   |-Acknowledge Number : %u\n",ntohl(tcph->ack_seq));
-    fprintf(logfile , "   |-Header Length      : %d DWORDS or %d BYTES\n" ,(unsigned int)tcph->doff,(unsigned int)tcph->doff*4);
-    //fprintf(logfile , "   |-CWR Flag : %d\n",(unsigned int)tcph->cwr);
-    //fprintf(logfile , "   |-ECN Flag : %d\n",(unsigned int)tcph->ece);
-//    fprintf(logfile , "   |-Urgent Flag          : %d\n",(unsigned int)tcph->urg);
-//    fprintf(logfile , "   |-Acknowledgement Flag : %d\n",(unsigned int)tcph->ack);
-//    fprintf(logfile , "   |-Push Flag            : %d\n",(unsigned int)tcph->psh);
-//    fprintf(logfile , "   |-Reset Flag           : %d\n",(unsigned int)tcph->rst);
-//    fprintf(logfile , "   |-Synchronise Flag     : %d\n",(unsigned int)tcph->syn);
-//    fprintf(logfile , "   |-Finish Flag          : %d\n",(unsigned int)tcph->fin);
-    fprintf(logfile , "   |-Window         : %d\n",ntohs(tcph->window));
-    fprintf(logfile , "   |-Checksum       : %d\n",ntohs(tcph->check));
-//    fprintf(logfile , "   |-Urgent Pointer : %d\n",tcph->urg_ptr);
-//    fprintf(logfile , "\n");
-//    fprintf(logfile , "                        DATA Dump                         ");
-//    fprintf(logfile , "\n");
-
-    fprintf(logfile , "IP Header\n");
-    PrintData(Buffer,iphdrlen);
-
-    fprintf(logfile , "TCP Header\n");
-    PrintData(Buffer+iphdrlen,tcph->doff*4);
-
-    fprintf(logfile , "Data Payload (size %d)\n", Size-header_size);
-    printf("Data Payload (size %d)\n", Size-header_size);
-//    PrintData(Buffer + header_size , Size - header_size );
-
-    fprintf(logfile , "\n###########################################################");
-}
-
-
-void print_icmp_packet(const u_char * Buffer , int Size)
-{
-    unsigned short iphdrlen;
-
-    struct iphdr *iph = (struct iphdr *)(Buffer  + sizeof(struct ethhdr));
-    iphdrlen = iph->ihl * 4;
-
-    struct icmphdr *icmph = (struct icmphdr *)(Buffer + iphdrlen  + sizeof(struct ethhdr));
-
-    int header_size =  sizeof(struct ethhdr) + iphdrlen + sizeof icmph;
-
-    fprintf(logfile , "\n\n***********************ICMP Packet*************************\n");
-
-    elaborate_and_print_ip_header(Buffer , Size);
-
-    fprintf(logfile , "\n");
-
-    fprintf(logfile , "ICMP Header\n");
-    fprintf(logfile , "   |-Type : %d",(unsigned int)(icmph->type));
-
-    if((unsigned int)(icmph->type) == 11)
-    {
-        fprintf(logfile , "  (TTL Expired)\n");
-    }
-    else if((unsigned int)(icmph->type) == ICMP_ECHOREPLY)
-    {
-        fprintf(logfile , "  (ICMP Echo Reply)\n");
-    }
-
-    fprintf(logfile , "   |-Code : %d\n",(unsigned int)(icmph->code));
-    fprintf(logfile , "   |-Checksum : %d\n",ntohs(icmph->checksum));
-    //fprintf(logfile , "   |-ID       : %d\n",ntohs(icmph->id));
-    //fprintf(logfile , "   |-Sequence : %d\n",ntohs(icmph->sequence));
-    fprintf(logfile , "\n");
-
-    fprintf(logfile , "IP Header\n");
-    PrintData(Buffer,iphdrlen);
-
-    fprintf(logfile , "UDP Header\n");
-    PrintData(Buffer + iphdrlen , sizeof icmph);
-
-    fprintf(logfile , "Data Payload\n");
-
-    //Move the pointer ahead and reduce the size of string
-    PrintData(Buffer + header_size , (Size - header_size) );
-
-    fprintf(logfile , "\n###########################################################");
-}
-
-void PrintData (const u_char * data , int Size)
-{
-    int i , j;
-    for(i=0 ; i < Size ; i++)
-    {
-        if( i!=0 && i%16==0)   //if one line of hex printing is complete...
-        {
-            fprintf(logfile , "         ");
-            for(j=i-16 ; j<i ; j++)
-            {
-                if(data[j]>=32 && data[j]<=128)
-                    fprintf(logfile , "%c",(unsigned char)data[j]); //if its a number or alphabet
-
-                else fprintf(logfile , "."); //otherwise print a dot
-            }
-            fprintf(logfile , "\n");
-        }
-
-        if(i%16==0) fprintf(logfile , "   ");
-        fprintf(logfile , " %02X",(unsigned int)data[i]);
-
-        if( i==Size-1)  //print the last spaces
-        {
-            for(j=0;j<15-i%16;j++)
-            {
-                fprintf(logfile , "   "); //extra spaces
-            }
-
-            fprintf(logfile , "         ");
-
-            for(j=i-i%16 ; j<=i ; j++)
-            {
-                if(data[j]>=32 && data[j]<=128)
-                {
-                    fprintf(logfile , "%c",(unsigned char)data[j]);
-                }
-                else
-                {
-                    fprintf(logfile , ".");
-                }
-            }
-
-            fprintf(logfile ,  "\n" );
-        }
-    }
 }
